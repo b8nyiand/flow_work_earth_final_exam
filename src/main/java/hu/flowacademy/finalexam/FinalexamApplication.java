0c85ba026f26445fc10af2c68c62e88cf830be64
Pixi13139 <istvan.raffai@flowacademy.hu>
2024-03-06 17:47:37 +0100

    packages, entity, bean

--- src/main/java/hu/flowacademy/finalexam/FinalexamApplication.java
+++ src/main/java/hu/flowacademy/finalexam/FinalexamApplication.java
@@ -1,13 +1,34 @@
 package hu.flowacademy.finalexam;
 
+import hu.flowacademy.finalexam.model.Employee;
+import hu.flowacademy.finalexam.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class FinalexamApplication {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalexamApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			if (employeeRepository.findAll().isEmpty()) {
+				Employee employee1 = new Employee("Tapsi", "Hapsi Tapsi", "Looney Tunes", 5, 1_233_564l, LocalDate.now());
+				employeeRepository.save(employee1);
+
+			}
+		};
+	}
 }
---
+++ src/main/java/hu/flowacademy/finalexam/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package hu.flowacademy.finalexam.service;
+
+public class EmployeeService {
+}
