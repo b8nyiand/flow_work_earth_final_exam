51a85761b9634fcfc8e5e4bd32133382bff0ef54
Oliver <oliver.markovic@flowacademy.com>
2024-03-06 18:21:04 +0100

    controller

--- src/main/java/hu/flowacademy/finalexam/controller/EmployeeController.java
+++ src/main/java/hu/flowacademy/finalexam/controller/EmployeeController.java
@@ -1,4 +1,29 @@
 package hu.flowacademy.finalexam.controller;
 
+import hu.flowacademy.finalexam.model.Employee;
+import hu.flowacademy.finalexam.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/get-username")
+    public List<Employee> userName() {
+        return employeeRepository.findByFullNameLike("Molnar%");
+    }
+
+    @GetMapping("/above-one-mill")
+    public List<Employee> higherThan1Mill(){
+        return employeeRepository.findBySalaryGreaterThan(500000);
+    }
+
+
 }
--- src/main/java/hu/flowacademy/finalexam/repository/EmployeeRepository.java
+++ src/main/java/hu/flowacademy/finalexam/repository/EmployeeRepository.java
@@ -4,5 +4,13 @@ import hu.flowacademy.finalexam.model.Employee;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
+
+    Employee findByUsername(String username);
+
+    List<Employee> findByFullNameLike(String fullName);
+
+    List<Employee> findBySalaryGreaterThan(int salary);
 }
