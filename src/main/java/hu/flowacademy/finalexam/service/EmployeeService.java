b39d6af8f50190729bf76af137449b1bc8077e0f
norbert lazar <norbert.lazar@flowacademy.hu>
2024-03-06 18:36:31 +0100

    fix time registration

--- src/main/java/hu/flowacademy/finalexam/service/EmployeeService.java
+++ src/main/java/hu/flowacademy/finalexam/service/EmployeeService.java
@@ -5,6 +5,7 @@ import hu.flowacademy.finalexam.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,10 +16,16 @@ public class EmployeeService {
     private EmployeeRepository employeeRepository;
 
     public Employee add(Employee employee){
+        if (employee.getCreationDate() == null) {
+            employee.setCreationDate(LocalDate.now());
+        }
         return employeeRepository.save(employee);
     }
 
     public Employee modify(Employee employee){
+        if (employee.getCreationDate() == null) {
+            employee.setCreationDate(LocalDate.now());
+        }
         return employeeRepository.save(employee);
     }
 
